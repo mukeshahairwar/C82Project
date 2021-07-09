2bc0d6c2818eb79ee890fdce8326ba9c56d2a1a2
mukeshahairwar <77151840+mukeshahairwar@users.noreply.github.com>
2021-07-09 16:29:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
-var mouseEvent = "empty";
- var last_position_of_x, last_position_of_y;
  
- canvas = document.getElementById('myCanvas');
-  ctx = canvas.getContext("2d");
-  
-  var color = "black";
-  var width_of_line = 3;
-
- canvas.addEventListener("mousedown", my_mousedown);
-  function my_mousedown(e) {
-       color = document.getElementById("color").value;
-        width_of_line = document.getElementById("width_of_line").value;
-        radius = document.getElementById("radius")
-
-      mouseEvent = "mouseDown";
+ canvas =document.getElementById("myCanvas");
+ ctx =canvas.getContext("2d");
+ var mouseEvent ="empty";
+ color ="red";
+ 
+ canvas.addEventListener("mousedown",
+                         mymousedown);
+ 
+ function mymousedown(e) {
+   mouseEvent="mouseDown";
+ }
+ canvas.addEventListener("mouseleave",mymouseleave);
+ function mymouseleave(e){
+    mouseEvent="mouseleave";
+ }
+ canvas.addEventListener("mouseup",mymouseup);
+ function mymouseup(e){
+    mouseEvent ="mouseUP";
+ }
+ canvas.addEventListener("mousemove",mymousemove);
+ function mymousemove(e){
+   current_x = e.clientX-canvas.offsetLeft;
+   current_y = e.clientY-canvas.offsetTop;
+       if (mouseEvent =="mouseDown") {
+        ctx.beginPath();
+        ctx.strokeStyle =color;
+        ctx.lineWidth = 1;
+       ctx.arc(current_x,current_y,20,0,
+               2*Math.PI);
+       
+        ctx.stroke(); 
      }
-     canvas.addEventListener("mousemove", my_mousemove);
-      function my_mousemove(e) {
-           current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
-            current_position_of_mouse_y = e.clientY - canvas.offsetTop;
-            
-            if (mouseEvent == "mouseDown") { 
-            
-            console.log("current position of x and y coordinates")
-            console.log("x = " + current_position_of_mouse_x + " y = " +  current_position_of_mouse_y )
-
-             ctx.beginPath();
-             ctx.strokeStyle = color;
-             ctx.lineWidth = width_of_line ;
-             ctx.arc(current_position_of_mouse_x,current_position_of_mouse_y,radius,0,2*Math.PI);
-             ctx.Stroke;
-                     }
-                      last_position_of_x = current_position_of_mouse_x;
-                       last_position_of_y = current_position_of_mouse_y; 
-                    }
+    
+ }
 
                     canvas.addEventListener("mouseup", my_mouseup);
                      function my_mouseup(e) 
